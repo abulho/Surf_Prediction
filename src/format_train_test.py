5f0527bcb00aa0a45331637d58eda42e17225fc6
Abdulla Mohmed <abdulla.aecom@gmail.com>
2018-01-28 20:23:20 -0800

    added function to make data hourly

--- src/format_train_test.py
+++ src/format_train_test.py
@@ -71,6 +71,31 @@ def round_time_y(dataframe):
                                                                                      dt.hour,
                                                                                      0,0))
     return dataframe
+    
+def make_hourly_data(dataframe, year):
+    '''
+    input:
+    DataFrame
+    year
+
+    output:
+    returns a data frame with a constant frequency of points,
+    will add points and interpolate to make a full hourly data set for
+    the given year.
+
+    '''
+    d = pd.date_range(start = '1/1/{} 00:00:00'.format(year),
+                      end   = '12/31/{} 23:00:00'.format(year),
+                      freq  = '1H')
+
+    df_test = pd.DataFrame(np.nan, index = d, columns = ['dummy'])
+    df_test['id'] = df_test.index
+
+    dataframe['id'] = dataframe.index
+    df_joined = pd.merge(df_test, dataframe, how='left', left_on='id', right_on='id')
+    df_joined = df_joined.interpolate(method='linear', axis=0).bfill()
+
+    return df_joined
 
 if __name__ == '__main__':
     filename = '../data/train_X_y.csv'
