a575194d318340990f7080e18d69751ebcbb2514
Abdulla Mohmed <abdulla.aecom@gmail.com>
2018-02-06 12:25:40 -0800

    deleted flask app  to make a new app

--- src/DASH_real_time_future_predictions.py
+++ src/DASH_real_time_future_predictions.py
@@ -7,6 +7,18 @@ matplotlib.use('agg')
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 
+all_pred_df = pd.read_csv('predictions_7days.csv', parse_dates=['Date'], index_col='Date')
+all_pred_df = all_pred_df.resample('H').mean()
+
+# adding an hour col to the df to parse it by 6am, 12pm and 6pm
+all_pred_df['date_fig'] = all_pred_df.index
+all_pred_df['hr_fig'] = all_pred_df['date_fig'].apply(lambda x: x.hour)
+bar_yy = all_pred_df[(all_pred_df['hr_fig']==6) | (all_pred_df['hr_fig']==12) | (all_pred_df['hr_fig']==18)]
+bar_yy = all_pred_df[(all_pred_df['hr_fig']==6) | (all_pred_df['hr_fig']==12) | (all_pred_df['hr_fig']==18)]
+
+# interpolating to fill in the missing gaps
+bar_yy = bar_yy.interpolate(method='linear', axis=0).bfill()
+
 def make_plot_times():
     dt = datetime.now().date()
     time_start = dt + timedelta(hours = 0)
@@ -23,6 +35,8 @@ def make_dash_board():
     graphic for dash board
 
     '''
+    print('Making Dash board...')
+
     myFmt = mdates.DateFormatter('%H:%S')
 
     time_start, time_end = make_plot_times()
@@ -30,10 +44,7 @@ def make_dash_board():
     t1 = time_start.strftime('%Y%m%d')
     t2 = time_end.strftime('%Y%m%d')
 
-    print(t1)
-    print(t2)
-
-    fig, ax = plt.subplots(figsize=(18,6))
+    fig, ax = plt.subplots(figsize=(25,6))
     ax.bar(bar_yy[t1:t2]['yhat'].index.to_pydatetime(),
            bar_yy[t1:t2]['yhat'].values,
            width=0.24,
@@ -57,7 +68,7 @@ def make_dash_board():
     for j, i in enumerate(ax.patches[0::3]):
         # get_x pulls left or right; get_height pushes up or down
         ax.text(i.get_x()+0.13, 4.6, \
-                weekday_dict[bar_yy[t1:t2]['yhat'][0::3].index[j].weekday()], fontsize=15,
+                weekday_dict[bar_yy[t1:t2]['yhat'][0::3].index[j].weekday()], fontsize=20,
                     color='black')
 
     for j, i in enumerate(ax.patches[0::3]):
@@ -71,7 +82,7 @@ def make_dash_board():
 
     ax.tick_params(axis='x', labelsize=18, color='black')
     ax.tick_params(axis='y', labelsize=18, color= 'black')
-    ax.set_ylabel('Wave Height (m)', fontsize=18, color='black')
+    ax.set_ylabel('Wave Height (m)', fontsize=20, color='black')
     ax.tick_params(axis='x', colors='black')
     ax.tick_params(axis='y', colors='black')
     ax.set_facecolor('white')
@@ -80,19 +91,11 @@ def make_dash_board():
     ax.spines['right'].set_color('blue')
     ax.spines['left'].set_color('blue')
     plt.tight_layout()
-    plt.savefig('DASH_BOARD.png')
+    plt.savefig('../FlaskApp/surfapp-1/static/DASH_BOARD.png')
+
+    print('Complete.')
+
+    return plt
 
 if __name__ == "__main__":
-    all_pred_df = pd.read_csv('predictions_7days.csv', parse_dates=['Date'], index_col='Date')
-    all_pred_df = all_pred_df.resample('H').mean()
-
-    # adding an hour col to the df to parse it by 6am, 12pm and 6pm
-    all_pred_df['date_fig'] = all_pred_df.index
-    all_pred_df['hr_fig'] = all_pred_df['date_fig'].apply(lambda x: x.hour)
-    bar_yy = all_pred_df[(all_pred_df['hr_fig']==6) | (all_pred_df['hr_fig']==12) | (all_pred_df['hr_fig']==18)]
-    bar_yy = all_pred_df[(all_pred_df['hr_fig']==6) | (all_pred_df['hr_fig']==12) | (all_pred_df['hr_fig']==18)]
-
-    # interpolating to fill in the missing gaps
-    bar_yy = bar_yy.interpolate(method='linear', axis=0).bfill()
-
     make_dash_board()
---
+++ src/main.py
@@ -0,0 +1,6 @@
+import os
+import making_7day_predictions
+import DASH_real_time_future_predictions
+
+os.system('python making_7day_predictions.py')
+os.system('python DASH_real_time_future_predictions.py')
